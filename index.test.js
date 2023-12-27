c5d139bdf679528a1bba328482a6a233e56b5b4c
TangiresH <dimon4ik1010@gmail.com>
2023-12-28 00:33:31 +0200

    added new tests

--- index.test.js
+++ index.test.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const { parse, validateInput } = require('./components/parse');
+const handleKeyPress = require('./components/handleKeyPress');
+
 
 describe('Input functions', () => {
   describe('Parse function', () => {
@@ -34,9 +36,12 @@ describe('Input functions', () => {
         expect(parse(inputString)).toEqual(["2", "5", "-", "1", "0", "="]);
       });
     });
+    
+  });
+  
+  
+  describe('Parse function', () => {
 
-
-    describe('Parse function', () => {
       test('should return null and log an error message for input with unexpected characters', () => {
         const inputFilePath = path.join(__dirname, './test-inputs/test-validateInput/test-input-file-1.txt');
         const inputString = fs.readFileSync(inputFilePath, 'utf-8');
@@ -85,10 +90,92 @@ describe('Input functions', () => {
         const result = validateInput(inputString);
         expect(result).toBe(true);
       });
-      
-      
-
-
 
     });
-  });
+
+    describe('handleKeyPress function', () => {
+      let calculatorState;
+
+      beforeEach(() => {
+        calculatorState = {
+          screen: 0,
+          op: null,
+          first_number: null,
+          start_second_number: false,
+        };
+      });
+
+      test('should set operator and start second number when pressing + key', () => {
+        handleKeyPress(calculatorState, '+');
+        expect(calculatorState.op).toEqual('+');
+        expect(calculatorState.start_second_number).toBe(true);
+        expect(calculatorState.first_number).toEqual(calculatorState.screen);
+      });
+
+      test('should handle numeric key when start_second_number is true', () => {
+        calculatorState.start_second_number = true;
+        handleKeyPress(calculatorState, '5');
+        expect(calculatorState.screen).toBe(5);
+        expect(calculatorState.start_second_number).toBe(false);
+      });
+
+      test('should handle numeric key when start_second_number is false', () => {
+        handleKeyPress(calculatorState, '3');
+        expect(calculatorState.screen).toBe(3);
+      });
+
+      test('should handle invalid key', () => {
+        handleKeyPress(calculatorState, 'A');
+        expect(calculatorState.screen).toBe(0);
+      });
+
+      test('should handle operator key with valid screen value', () => {
+        calculatorState.screen = 7;
+        handleKeyPress(calculatorState, '-');
+        expect(calculatorState.op).toBe('-');
+        expect(calculatorState.start_second_number).toBe(true);
+        expect(calculatorState.first_number).toBe(7);
+      });
+
+      test('should handle numeric keys sequentially', () => {
+        handleKeyPress(calculatorState, '1');
+        handleKeyPress(calculatorState, '2');
+        handleKeyPress(calculatorState, '3');
+        expect(calculatorState.screen).toBe(123);
+      });
+
+      test('should handle a complex expression with + and = keys', () => {
+        handleKeyPress(calculatorState, '1');
+        handleKeyPress(calculatorState, '+');
+        handleKeyPress(calculatorState, '2');
+        handleKeyPress(calculatorState, '=');
+        expect(calculatorState.screen).toBe(3);
+      });
+
+      test('should handle a complex expression with - and = keys', () => {
+        handleKeyPress(calculatorState, '7');
+        handleKeyPress(calculatorState, '-');
+        handleKeyPress(calculatorState, '2');
+        handleKeyPress(calculatorState, '=');
+        expect(calculatorState.screen).toBe(5);
+      });
+
+      test('should handle a complex expression with * and = keys', () => {
+        handleKeyPress(calculatorState, '3');
+        handleKeyPress(calculatorState, '*');
+        handleKeyPress(calculatorState, '4');
+        handleKeyPress(calculatorState, '=');
+        expect(calculatorState.screen).toBe(12);
+      });
+
+      test('should handle a complex expression with / and = keys', () => {
+        handleKeyPress(calculatorState, '1');
+        handleKeyPress(calculatorState, '0');
+        handleKeyPress(calculatorState, '/');
+        handleKeyPress(calculatorState, '2');
+        handleKeyPress(calculatorState, '=');
+        expect(calculatorState.screen).toBe(5);
+      });
+
+      
+    });
